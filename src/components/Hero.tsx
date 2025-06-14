e988dc8c9de9b109fa3117adf3c2b74bae18e619
renan-disparopro <renan@disparopro.com.br>
2025-06-14 18:06:50 -0300

    refactor: remove animations.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,12 +1,13 @@
+
 const Hero = () => {
-  return (
-    <div className="flex flex-col items-center justify-center flex-1 text-center px-4">
-      <h2 className="text-4xl md:text-6xl font-bold">Renan Johny Ortega</h2>
-      <p className="mt-4 text-4xl md:text-6xl text-gray-400 font-bold">
-        Design. Develop. Inspire.
-      </p>
-    </div>
-  );
+    return (
+        <div className="flex flex-col items-center justify-center flex-1 text-center px-4">
+            <h2 className="text-4xl md:text-6xl font-bold">Renan Johny Ortega</h2>
+            <p className="mt-4 text-4xl md:text-6xl text-gray-400 font-bold">
+                Design. Develop. Inspire.
+            </p>
+        </div>
+    );
 };
 
 export default Hero;
