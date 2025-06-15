43ecfc3b6bfe93b292d2ed5370caaa13be34a762
renan-disparopro <renan@disparopro.com.br>
2025-06-15 00:44:49 -0300

    feat: add hero text animation.

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,14 @@
+import { FlipWords } from "./ui/flip-words";
 
 const Hero = () => {
+    const words = ["Design.", "Develop.", "Inspire."];
+
     return (
-        <div className="flex flex-col items-center justify-center flex-1 text-center px-4">
+        <div className="flex flex-col gap-3 items-center justify-center flex-1 text-center px-4">
             <h2 id="teste" className="text-4xl md:text-6xl font-bold">Renan Johny Ortega</h2>
-            <p className="mt-4 text-4xl md:text-6xl text-gray-400 font-bold">
-                Design. Develop. Inspire.
-            </p>
+            <div className="p-2 w-50 md:w-70 text-4xl md:text-6xl bg-black font-bold">
+                <FlipWords words={words} />
+            </div>
         </div>
     );
 };
