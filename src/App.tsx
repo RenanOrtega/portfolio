5cdaf0182ab6b078becf3b2f026d078bca310a15
renan-disparopro <renan@disparopro.com.br>
2025-06-15 12:23:34 -0300

    feat: add reactbits silk.

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Silk from "./blocks/Backgrounds/Silk/Silk"
 import Collaborate from "./components/Collaborate"
 import Footer from "./components/Footer"
 import Hero from "./components/Hero"
@@ -9,40 +10,54 @@ import { TracingBeam } from "./components/ui/tracing-beam"
 
 function App() {
   return (
-    <div className="px-8 py-6 md:px-30">
-      {/* Hero Section */}
-      <Navbar />
-      <TracingBeam
-        className="pb-20"
-      >
-        <section className="min-h-screen flex flex-col justify-center pt-20">
-          <Hero />
-        </section>
-
-        <div className="mx-auto antialiased relative">
-          {/* Projects Section */}
-          <section className="min-h-screen flex flex-col" id="projects">
-            <SectionDescription
-              description="A selection of my favorite work across design and development, with a focus on impactful, modern solutions."
-            />
-            <Projects />
-          </section>
-
-          {/* Skills Section */}
-          <section className="min-h-screen flex flex-col justify-center" id="skills">
-            <Skills />
-          </section>
-
-          {/* Contact Section */}
-          <section className="min-h-screen flex flex-col justify-center" id="contact">
-            <Collaborate />
-          </section>
+    <>
+      <section className="min-h-screen relative flex flex-col">
+        <div className="absolute inset-0 w-full h-full z-0">
+          <Silk
+            speed={5}
+            scale={1}
+            color="#7B7481"
+            noiseIntensity={1.5}
+            rotation={0}
+          />
         </div>
-      </TracingBeam>
+        <div className="relative z-10 flex flex-col h-full min-h-screen">
+          <div className="px-8 py-6 md:px-30">
+            <Navbar />
+          </div>
+          {/* Hero ocupa o espaço restante */}
+          <div className="flex-1 flex items-center justify-center">
+            <Hero />
+          </div>
+        </div>
+      </section>
+      <div className="px-8 py-6 md:px-30">
+        <TracingBeam className="pb-20">
+          <div className="mx-auto antialiased relative">
+            {/* Projects Section */}
+            <section className="min-h-screen flex flex-col" id="projects">
+              <SectionDescription
+                description="A selection of my favorite work across design and development, with a focus on impactful, modern solutions."
+              />
+              <Projects />
+            </section>
 
-      {/* Footer */}
-      <Footer />
-    </div>
+            {/* Skills Section */}
+            <section className="min-h-screen flex flex-col justify-center" id="skills">
+              <Skills />
+            </section>
+
+            {/* Contact Section */}
+            <section className="min-h-screen flex flex-col justify-center" id="contact">
+              <Collaborate />
+            </section>
+          </div>
+        </TracingBeam>
+
+        {/* Footer */}
+        <Footer />
+      </div>
+    </>
   )
 }
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,8 @@
 const Navbar = () => {
     return (
         <header className="flex justify-between items-center">
-            <h1 className="font-bold text-lg">Renan<span className="text-xs">®</span></h1>
-            <nav className="flex gap-8 text-gray-500">
+            <h1 className="font-bold text-lg text-white">Renan<span className="text-xs">®</span></h1>
+            <nav className="flex gap-8 text-white">
                 <a href="#projects" className="hover:text-black">Projects</a>
                 <a href="#skills" className="hover:text-black">Skills</a>
                 <a href="#contact" className="hover:text-black">Contact</a>
